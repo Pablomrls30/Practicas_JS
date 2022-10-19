dad91517f5c0f56dbf1f5ab2ca50f001ce9d7317
Pablo Morales <pablomrls30@gmail.com>
2022-10-19 10:43:38 -0500

    JavaScript 1

---
+++ 1_js1.js
@@ -0,0 +1,11 @@
+console.log('Hola JavaScript');
+console.warn('Hola a todos');
+console.error('Saludos')
+
+console.log('El tipo de dato de false ${typeof false} ');
+console.log('El tipo de dato de false ${typeof 0} ');
+console.log('El tipo de dato de false ${typeof jji} ');
+console.log(`El resultado es: ${3 > 1 || 3 == 2}`);
+
+console.log(1 > 2 ? 'verdadero' : 'falso')
+const UNI = 'UANL';
